60e50c8e64e3b4b7d20c7a503aaff05a20aa5b92
FathyV <hung.tovi@gmail.com>
2016-11-23 20:17:13 +0700

    Upgrade the application
    Allow the user to continue after enter the name of the month.

--- HieuFaggotApp_12/HieuFaggotApp_12/Source.cpp
+++ HieuFaggotApp_12/HieuFaggotApp_12/Source.cpp
@@ -3,12 +3,23 @@
 using namespace std;
 
 bool input();
+bool isContinue();
+
+bool isContinue() {
+	string choice;
+	cout << "Do you want to continue[y/n]: ";
+	getline(cin, choice);
+	if (choice == "y")
+		return true;
+	else
+		return false;
+}
 
 bool input() {
 	string name;
-	string month[12] = {"January","February","March","April","May","June","July","August","September","October","November","December"};
+	string month[12] = {"january","february","march","april","may","june","july","august","september","october","november","december",};
 	try {
-		cout << "Please enter your name: ";
+		cout << "Please enter the month name: ";
 		getline(cin, name);
 		if (name == "") {
 			throw 1;
@@ -29,7 +40,8 @@ bool input() {
 			}
 			else {
 				cout << "The number of the month is: " << iMonth + 1<<endl;
-				return true;
+				
+				return isContinue();
 			}
 		}
 	}
@@ -40,15 +52,14 @@ bool input() {
 		else if(e==2){
 			cout << "You must enter the valid name!" << endl;
 		}
-
-		return true;
 	}
+
+	return isContinue();
 }
 
 int main() {
 	bool isContinue = true;
 	while (isContinue) {
 		isContinue= input();
-		cout << "Result is " << isContinue << endl;
 	}
 }
